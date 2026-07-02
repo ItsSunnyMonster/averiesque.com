d13d058cf4cf6e149e4810639c8f78be28780dde
Avery Lin <100400733+ItsSunnyMonster@users.noreply.github.com>
2026-07-02 16:52:58 +1200

    feat: add squishing to cursor

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -5,3 +5,7 @@ export function randomRange(min: number, max: number) {
 export function lerp(from: number, to: number, t: number) {
   return to * t + from * (1 - t);
 }
+
+export function distance(x1: number, y1: number, x2: number, y2: number) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
