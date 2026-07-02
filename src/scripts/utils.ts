ef3285b701d7ff19e16661f4babca8f6d62494ea
Avery Lin <100400733+ItsSunnyMonster@users.noreply.github.com>
2026-07-02 12:06:04 +1200

    fix: remove empty lerp vector function

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -5,9 +5,3 @@ export function randomRange(min: number, max: number) {
 export function lerp(from: number, to: number, t: number) {
   return to * t + from * (1 - t);
 }
-
-export function lerpVector2(
-  from: [number, number],
-  to: [number, number],
-  t: number,
-) {}
