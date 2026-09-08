c2edf404d4cb0b99a038d995ae03eb20aacadc6c
Avery Lin <avery@averiesque.com>
2026-09-08 20:47:35 +1200

    refactor: probProber is now a singleton

--- src/scripts/utils/propertyProber.ts
+++ src/scripts/utils/propertyProber.ts
@@ -1,28 +1,21 @@
-export class PropProber {
+class PropProber {
   private probe: HTMLDivElement;
 
   private cache: WeakMap<HTMLElement, Map<string, number>> = new WeakMap();
 
   constructor() {
-    this.probe = this.createProbe();
-  }
+    this.probe = document.createElement("div");
+    this.probe.style.position = "absolute";
+    this.probe.style.visibility = "hidden";
+    this.probe.style.pointerEvents = "none";
+    document.body.appendChild(this.probe);
 
-  private createProbe(): HTMLDivElement {
-    const probe = document.createElement("div");
-    probe.style.position = "absolute";
-    probe.style.visibility = "hidden";
-    probe.style.pointerEvents = "none";
-    document.body.appendChild(probe);
-    return probe;
+    document.addEventListener("astro:after-swap", () =>
+      document.body.appendChild(this.probe),
+    );
   }
 
   propertyToPx(el: HTMLElement, prop: string) {
-    if (!this.probe.isConnected) {
-      this.probe = this.createProbe();
-      // NOTE: don't need to clear the weak map here because once the element is dropped
-      // the weak map does not keep it alive and so it also gets garbage collected anyway
-    }
-
     let propToPxMap = this.cache.getOrInsert(el, new Map());
 
     return propToPxMap.getOrInsertComputed(prop, (prop: string) => {
@@ -46,3 +39,5 @@ export class PropProber {
     });
   }
 }
+
+export const propProber = new PropProber();
